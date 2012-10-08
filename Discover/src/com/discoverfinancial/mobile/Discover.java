6241d8f2220c6aad946d7d0f01394ddf8abe4910
Dave Henke <dhenk@me.com>
2012-10-08 15:16:56 -0500

    Very simple login layout

---
+++ Discover/src/com/discoverfinancial/mobile/Discover.java
@@ -0,0 +1,8 @@
+package com.discoverfinancial.mobile;
+
+import android.app.Application;
+import android.util.Log;
+
+public class Discover extends Application {
+
+}
---
+++ Discover/src/com/discoverfinancial/mobile/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.discoverfinancial.mobile.login;
+
+import com.discoverfinancial.mobile.R;	
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login);
+		
+	}
+}
