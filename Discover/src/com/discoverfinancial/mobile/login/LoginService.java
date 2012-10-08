013d3ee3f0b1f2e94af4de6226b51ae8c07b1fbd
Dave Henke <dhenk@me.com>
2012-10-08 16:06:34 -0500

    Added spring for android

---
+++ Discover/src/com/discoverfinancial/mobile/login/LoginService.java
@@ -0,0 +1,12 @@
+package com.discoverfinancial.mobile.login;
+
+import org.springframework.web.client.RestTemplate;
+
+public class LoginService {
+
+	public void login() {
+		RestTemplate restTemplate = new RestTemplate();
+		
+	}
+	
+}
