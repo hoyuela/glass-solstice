d7dd1feb60f9a74590aba015f2894f342d0891bc
Dave Henke <dhenk@me.com>
2012-10-08 16:51:25 -0500

    Added spring to login

--- Discover/src/com/discoverfinancial/mobile/login/LoginService.java
+++ Discover/src/com/discoverfinancial/mobile/login/LoginService.java
@@ -1,12 +1,39 @@
 package com.discoverfinancial.mobile.login;
 
+import org.springframework.http.HttpAuthentication;
+import org.springframework.http.HttpBasicAuthentication;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
+import org.springframework.util.support.Base64;
 import org.springframework.web.client.RestTemplate;
 
 public class LoginService {
-
-	public void login() {
+	private static String url = "";
+	public void login(String username, String password) {
 		RestTemplate restTemplate = new RestTemplate();
-		
+		restTemplate.getMessageConverters().add(new MappingJacksonHttpMessageConverter());
+		restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+		HttpHeaders requestHeaders = new HttpHeaders();
+		HttpAuthentication auth = authenticationHeader(username, password);
+		requestHeaders.setAuthorization(auth);
 	}
-	
+	private HttpAuthentication authenticationHeader(final String username,
+			final String password) {
+		return new HttpAuthentication() {
+			@Override
+			public String getHeaderValue() {
+				StringBuilder sb = new StringBuilder();
+				sb.append(Base64.encodeBytes(username.getBytes()));
+				sb.append("\": :\"");
+				sb.append(Base64.encodeBytes(password.getBytes()));
+				return sb.toString();
+			}
+		};
+	}
+
 }
