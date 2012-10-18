4f14aa360ec564abaa26083c78dda698797dc2e3
Ryan Sander <rsander@solstice-consulting.com>
2012-10-18 16:09:38 -0500

    Rearranging project structure, Initial commit for general service class

--- Commons/main/src/com/discover/mobile/common/auth/AuthService.java
+++ Commons/main/src/com/discover/mobile/common/auth/AuthService.java
@@ -1,6 +1,8 @@
-package com.discover.mobile.commons;
+package com.discover.mobile.common.auth;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -12,14 +14,14 @@ import org.apache.http.impl.client.DefaultHttpClient;
 
 import android.util.Log;
 
-public class LoginLogout {
+public class AuthService {
 	
 	// TODO temporary
 	private static final ExecutorService executor = Executors.newSingleThreadExecutor();
 	
 	private static final String TAG = "LoginLogout";
 	
-	public void preAuthCheck() {
+	public void preAuthCheckOld() {
 		executor.submit(new Runnable() {
 
 			@Override
@@ -45,6 +47,43 @@ public class LoginLogout {
 		            e.printStackTrace();
 		        } catch (IOException e) {
 		            client.getConnectionManager().shutdown();
+		            e.printStackTrace();
+		        }
+			}
+			
+		});
+	}
+	
+	public void preAuthCheck() {
+		executor.submit(new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				
+				HttpURLConnection urlConnection = null;
+				URL url;
+				int responseCode;
+				
+				try {
+					url = new URL("https://www.discovercard.com/cardsvcs/acs/session/preauthcheck");
+					
+					urlConnection = (HttpURLConnection) url.openConnection();
+					urlConnection.setRequestMethod("GET");
+					urlConnection.setRequestProperty("X-Client-Platform", "Android");
+					urlConnection.setRequestProperty("X-Application-Version", "4.00");
+					
+					urlConnection.setReadTimeout(15*1000);
+					urlConnection.connect();
+					
+		            responseCode = urlConnection.getResponseCode();
+		            
+		            Log.d(TAG, "Response code: " + responseCode);
+		        } catch (ClientProtocolException e)  {
+		            urlConnection.disconnect();
+		            e.printStackTrace();
+		        } catch (IOException e) {
+		            urlConnection.disconnect();
 		            e.printStackTrace();
 		        }
 			}
