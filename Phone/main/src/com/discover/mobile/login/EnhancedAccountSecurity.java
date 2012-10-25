61f8905587d2505ce32a1f6b89c9ce45c95c36a0
Scott Seward <sseward@solstice-mobile.com>
2012-10-25 16:26:48 -0500

    Screen 1.5 is up and running. It's the intermediate screen between step 1 and 2 of the account info entry for creating a new user account.

--- Phone/main/src/com/discover/mobile/login/EnhancedAccountSecurity.java
+++ Phone/main/src/com/discover/mobile/login/EnhancedAccountSecurity.java
@@ -2,15 +2,41 @@ package com.discover.mobile.login;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.discover.mobile.R;
 
 public class EnhancedAccountSecurity extends Activity{
 	
+	private TextView detailHelpLabel, statusIconLabel;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.enhanced_account_security);
+	}
+	
+	@Override
+	public void onStart(){
+		super.onStart();
+		setupToggleItems();
+	}
+	private void setupToggleItems(){
+		detailHelpLabel = (TextView)findViewById(R.id.account_security_whats_this_detail_label);
+		statusIconLabel = (TextView)findViewById(R.id.account_security_plus_label);
+
+	}
+	
+	public void expandHelpMenu(View v){
+			if(statusIconLabel.getText().equals("+")){
+				statusIconLabel.setText(getString(R.string.account_security_minus_text));
+				detailHelpLabel.setMaxLines(10);
+			}
+			else{
+				statusIconLabel.setText(getString(R.string.account_security_plus_text));
+				detailHelpLabel.setMaxLines(0);
+			}
 	}
 
 }
