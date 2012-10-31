d3b7085c2aeed03353985f3ba80eb80c181c29de
Scott Seward <sseward@solstice-mobile.com>
2012-10-31 11:54:41 -0500

    Added confirmation page. Almost complete I think.

---
+++ Phone/main/src/com/discover/mobile/register/AccountInformationConfirmation.java
@@ -0,0 +1,14 @@
+package com.discover.mobile.register;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AccountInformationConfirmation extends Activity{
+	
+	@Override
+	public void onCreate(final Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		
+	}
+
+}
