a1d1d073f5db7bec6a51c168b9c641dd373a43ba
Ryan Sander <rsander@solstice-consulting.com>
2012-11-05 14:57:27 -0600

    Added blank lock out user page & activity

---
+++ Phone/main/src/com/discover/mobile/login/LockOutUserActivity.java
@@ -0,0 +1,18 @@
+package com.discover.mobile.login;
+
+import roboguice.activity.RoboActivity;
+import roboguice.inject.ContentView;
+import android.os.Bundle;
+
+import com.discover.mobile.R;
+
+@ContentView(R.layout.lock_out_user)
+public class LockOutUserActivity extends RoboActivity {
+
+	@Override
+	protected void onCreate(final Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	
+}
