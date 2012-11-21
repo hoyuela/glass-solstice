df3ba4e6cc425092d7fc803ab73b825935626eed
Jon Thornton <jthornton@solstice-consulting.com>
2012-11-21 10:56:40 -0600

    initial Commit of the test code plc

--- Commons/main/src/com/discover/mobile/common/analytics/TrackingHelper.java
+++ Commons/main/src/com/discover/mobile/common/analytics/TrackingHelper.java
@@ -7,8 +7,8 @@ import com.adobe.adms.measurement.ADMS_Measurement;
 public final class TrackingHelper {
 	
 	// FIXME externalize
-	private static final String TRACKING_RSID = "discovercardmobiledev";
-	private static final String TRACKING_SERVER = "discoverfinancial.d1.sc.omtrdc.net";
+	private static final String TRACKING_RSID = "discovercardmobiledev"; //$NON-NLS-1$
+	private static final String TRACKING_SERVER = "discoverfinancial.d1.sc.omtrdc.net"; //$NON-NLS-1$
 	// params for using Bloodhound
 //	private static final String TRACKING_RSID = "test";
 //	private static final String TRACKING_SERVER = "192.168.4.235:50046";
@@ -33,13 +33,16 @@ public final class TrackingHelper {
 	}
 	
 	public static void trackPageView(final String pageName) {
+		
+		//TODO:  THis is here to prevent a null pinter during testing
+		if(measurement == null){return;}
 		measurement.clearVars();
 		measurement.setAppState(pageName);
 		measurement.track();
 	}
 	
 	private TrackingHelper() {
-		throw new UnsupportedOperationException("This class is non-instantiable");
+		throw new UnsupportedOperationException("This class is non-instantiable"); //$NON-NLS-1$
 	}
 	
 }
