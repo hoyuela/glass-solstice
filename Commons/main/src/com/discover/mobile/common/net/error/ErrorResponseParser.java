06ddf1adff4f61d3ac1b63bda381d2006f18a8ab
Jon Thornton <jthornton@solstice-consulting.com>
2012-12-06 13:31:30 -0600

    Fixing maven issues

--- Commons/main/src/com/discover/mobile/common/net/error/ErrorResponseParser.java
+++ Commons/main/src/com/discover/mobile/common/net/error/ErrorResponseParser.java
@@ -4,8 +4,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 
+//Can define this directly (implement)
 public interface ErrorResponseParser<E extends ErrorResponse<?>> {
 	
+	//Should return null if it cannot be parsed
 	E parseErrorResponse(int httpStatusCode, InputStream in, HttpURLConnection conn) throws IOException;
 	
 }
