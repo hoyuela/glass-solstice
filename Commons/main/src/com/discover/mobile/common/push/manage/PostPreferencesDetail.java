7e98dcd1623a0d71649137f5e794f83fc9b851cc
Jon Thornton <jthornton@solstice-consulting.com>
2012-12-27 08:38:43 -0600

    US3822: Committing merge issues.

--- Commons/main/src/com/discover/mobile/common/push/manage/PostPreferencesDetail.java
+++ Commons/main/src/com/discover/mobile/common/push/manage/PostPreferencesDetail.java
@@ -32,8 +32,6 @@ public class PostPreferencesDetail implements Serializable{
 
 	/**String representing the user acceptance of the T&C is pending*/
 	public static final String PENDING = "P";
-	
-	public static final String DECLINE = "N";
 
 	/**String representing the user declined the T&C*/
 	public static final String DECLINE = "N";
