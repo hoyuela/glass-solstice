a3dd8684a00374648af3542068322ec5e8eeb4c1
Scott Seward <sseward@solstice-mobile.com>
2013-01-01 22:41:19 -0600

    Added javadoc to CustomDatePickerDialog for code review.

--- Commons/main/src/com/discover/mobile/common/customui/CustomDatePickerDialog.java
+++ Commons/main/src/com/discover/mobile/common/customui/CustomDatePickerDialog.java
@@ -8,9 +8,20 @@ import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
 
+/**
+ * This custom date picker extends the default android date picker dialog.
+ * It is extended in this case for the purpose of removing the day spinner from the dialog.
+ * This class simply hides the day picker and only presents the month and year.
+ * 
+ * @author scottseward
+ *
+ */
 public class CustomDatePickerDialog extends DatePickerDialog{
 	private static final String TAG = CustomDatePickerDialog.class.getSimpleName();
 	
+	/**
+	 * Default constructors.
+	 */
 	public CustomDatePickerDialog(Context context, int theme,
 			OnDateSetListener callBack, int year, int monthOfYear,
 			int dayOfMonth) {
@@ -21,12 +32,20 @@ public class CustomDatePickerDialog extends DatePickerDialog{
 			int dayOfMonth) {
 		super(context, callBack, year, monthOfYear, dayOfMonth);
 	}
-	
-	@Override
-	public void onDateChanged(DatePicker view, int year, int month, int day) {
-		/*intentionally empty*/
-	}
 
+	/**
+	 * This method is overridden because the default behavior of the date picker dialog is to 
+	 * change its title as the date is changed. We want a static title. So it's overridden with
+	 * no method body.
+	 */
+	@Override
+	public void onDateChanged(DatePicker view, int year, int month, int day) {/*intentionally empty*/}
+
+	/**
+	 * This method looks for declared variables in the DatePickerDialog class that match
+	 * known names for the day spinner. It then sets the visibility of the found element to GONE
+	 * so that we get a nice looking month and year date picker dialog.
+	 */
 	public void hideDayPicker() {
 		
 		 try{
