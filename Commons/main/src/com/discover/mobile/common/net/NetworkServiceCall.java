676533e29f21d6f4d472c588322b156b6c4ecc48
AJ Leeds <aleeds@solstice-mobile.com>
2013-01-18 08:10:22 -0600

    US4630 - Fixed the certificate issue. Currently it is set up to ignore the certificate in order to get past it.

--- Commons/main/src/com/discover/mobile/common/net/NetworkServiceCall.java
+++ Commons/main/src/com/discover/mobile/common/net/NetworkServiceCall.java
@@ -14,8 +14,17 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.List;
 import java.util.Map;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import android.content.Context;
 import android.os.Handler;
@@ -240,8 +249,54 @@ public abstract class NetworkServiceCall<R> {
 	
 	private HttpURLConnection createConnection() throws IOException {
 		final URL fullUrl = getFullUrl();
-		return (HttpURLConnection) fullUrl.openConnection();
+		if (fullUrl.getProtocol().toLowerCase().equals("https")) {
+	        trustAllHosts();
+	        HttpsURLConnection https = (HttpsURLConnection) fullUrl.openConnection();
+	        https.setHostnameVerifier(DO_NOT_VERIFY);
+	        conn = https;
+	    } else {
+	        conn = (HttpURLConnection) fullUrl.openConnection();
+	    }
+		return conn;
 	}
+	
+	// always verify the host - dont check for certificate
+    final static HostnameVerifier DO_NOT_VERIFY = new HostnameVerifier() {
+          public boolean verify(String hostname, SSLSession session) {
+              return true;
+          }
+   };
+
+
+    /**
+     * Trust every server - dont check for any certificate
+     */
+    private static void trustAllHosts() {
+              // Create a trust manager that does not validate certificate chains
+              TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+                      public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                              return new java.security.cert.X509Certificate[] {};
+                      }
+
+                      public void checkClientTrusted(X509Certificate[] chain,
+                                      String authType) throws CertificateException {
+                      }
+
+                      public void checkServerTrusted(X509Certificate[] chain,
+                                      String authType) throws CertificateException {
+                      }
+              } };
+
+              // Install the all-trusting trust manager
+              try {
+                      SSLContext sc = SSLContext.getInstance("TLS");
+                      sc.init(null, trustAllCerts, new java.security.SecureRandom());
+                      HttpsURLConnection
+                                      .setDefaultSSLSocketFactory(sc.getSocketFactory());
+              } catch (Exception e) {
+                      e.printStackTrace();
+              }
+      }
 	
 	private URL getFullUrl() throws IOException {
 		return new URL(BASE_URL + params.path);
