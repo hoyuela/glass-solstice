fca3bc7382453132a8c9874b73ff8b98530ae02f
Samuel Frank Smith <samfranksmith@gmail.com>
2013-01-22 08:15:11 -0600

    Extracted some strings into a Account Codes and Types class

---
+++ Commons/main/src/com/discover/mobile/common/AccountCodes.java
@@ -0,0 +1,20 @@
+package com.discover.mobile.common;
+
+public class AccountCodes {
+
+	
+	/** Incentive Type for Miles Bonus */
+	public static final String INCENTIVE_TYPE_MI2 = "MI2";
+	
+	/** Incentive Type for Cashback Bonus */
+	public static final String INCENTIVE_TYPE_CBB = "CBB";
+	
+	/** Incentive Code for Escape Card */ 
+	public static final String INCENTIVE_CODE_ESC = "000003";
+	
+	/** Card Type for Consumers/Personal */
+	public static final String CARD_TYPE_PERSONAL = "000001";
+	
+	/** Card Product Group Code for Escape */
+	public static final String CPGC_ESCAPE = "ESC";
+}
--- Commons/main/src/com/discover/mobile/common/CommonMethods.java
+++ Commons/main/src/com/discover/mobile/common/CommonMethods.java
@@ -2,8 +2,6 @@ package com.discover.mobile.common;
 
 import java.util.Locale;
 
-import com.discover.mobile.common.auth.AccountDetails;
-
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -17,12 +15,10 @@ import com.discover.mobile.common.auth.AccountDetails;
 public final class CommonMethods {
 	private final static String TAG = CommonMethods.class.getSimpleName();
 
-	// FIXME Duped code. See CommonUtils.showLabel(v). Remove this, or other.
 	public final static void setViewGone(View v) {
 		v.setVisibility(View.GONE);
 	}
 
-	// FIXME Duped code. See CommonUtils.showLabel(v). Remove this, or other.
 	public final static void setViewVisible(View v) {
 		v.setVisibility(View.VISIBLE);
 	}
@@ -46,38 +42,45 @@ public final class CommonMethods {
 	}
 
 	/**
-	 * Sets a given EditText's input to all lowercase characters. Useful when restricting the
-	 * input of a field.
+	 * Sets a given EditText's input to all lowercase characters. Useful when
+	 * restricting the input of a field.
 	 * 
 	 * @param input
 	 * @param field
 	 */
-	public static void setInputToLowerCase(final CharSequence input, final EditText field){
+	public static void setInputToLowerCase(final CharSequence input,
+			final EditText field) {
 		final String inputString = input.toString();
-		final String lowerCaseInput = inputString.toLowerCase(Locale.getDefault());
-		
-		if( !inputString.equals(lowerCaseInput)){
+		final String lowerCaseInput = inputString.toLowerCase(Locale
+				.getDefault());
+
+		if (!inputString.equals(lowerCaseInput)) {
 			field.setText(lowerCaseInput);
 			field.setSelection(lowerCaseInput.length());
 		}
-		
+
 	}
-	
+
 	/**
-	 * Set a text label visible and assign its text value to the given string resource.
+	 * Set a text label visible and assign its text value to the given string
+	 * resource.
 	 * 
-	 * @param label - A TextView to set visible and change the text of.
-	 * @param text - The String resource to resolve and present.
-	 * @param context - the context that is using this method.
+	 * @param label
+	 *            - A TextView to set visible and change the text of.
+	 * @param text
+	 *            - The String resource to resolve and present.
+	 * @param context
+	 *            - the context that is using this method.
 	 */
-	public final static void showLabelWithStringResource(final TextView label, final int text, final Context context) {
+	public final static void showLabelWithStringResource(final TextView label,
+			final int text, final Context context) {
 		label.setText(context.getResources().getString(text));
 		setViewVisible(label);
 	}
-	
+
 	/**
-	 * Launches the android native phone dialer with a given telephone number, and awaits user's
-	 * action to initiate the call.
+	 * Launches the android native phone dialer with a given telephone number,
+	 * and awaits user's action to initiate the call.
 	 * 
 	 * @param number
 	 *            - a String representation of a phone number to dial.
@@ -85,17 +88,18 @@ public final class CommonMethods {
 	 *            - When calling this method, pass it the context/activity that
 	 *            called this method.
 	 */
-	public final static void dialNumber(final String number, final Context callingContext) {
-		if(number != null && callingContext != null) {
+	public final static void dialNumber(final String number,
+			final Context callingContext) {
+		if (number != null && callingContext != null) {
 			Intent dialNumber = new Intent(Intent.ACTION_DIAL);
-			
+
 			dialNumber.setData(Uri.parse("tel:" + number));
-	
+
 			callingContext.startActivity(dialNumber);
-		}else{
+		} else {
 			Log.e(TAG, "ERROR : Attempting to dial a null number");
 		}
-		
+
 	}
 
 	/**
@@ -109,28 +113,32 @@ public final class CommonMethods {
 
 		// No Outage
 		if (!accountDetails.cardProductGroupOutageMode) {
-			if (accountDetails.cardProductGroupCode.equalsIgnoreCase("ESC")) {
+			if (accountDetails.cardProductGroupCode
+					.equalsIgnoreCase(AccountCodes.CPGC_ESCAPE)) {
 				return true;
 			}
 			return false;
 		}
 
 		// Outage - manual discernment of type
-		if (accountDetails.cardType.equalsIgnoreCase("000001")
-				&& accountDetails.incentiveCode.equalsIgnoreCase("000003")
-				&& accountDetails.incentiveTypeCode.equalsIgnoreCase("MI2")) {
+		if (accountDetails.cardType
+				.equalsIgnoreCase(AccountCodes.CARD_TYPE_PERSONAL)
+				&& accountDetails.incentiveCode
+						.equalsIgnoreCase(AccountCodes.INCENTIVE_CODE_ESC)
+				&& accountDetails.incentiveTypeCode
+						.equalsIgnoreCase(AccountCodes.INCENTIVE_TYPE_MI2)) {
 			return true;
 		}
 		return false;
 	}
-	
+
 	/**
 	 * Search through a String and remove any spaces
 	 */
 	public final static String getSpacelessString(final String stringWithSpaces) {
 		String stringWithNoSpaces = stringWithSpaces;
 
-		if(stringWithSpaces != null) {
+		if (stringWithSpaces != null) {
 			stringWithNoSpaces = stringWithSpaces.replace(" ", "");
 		}
 
@@ -138,18 +146,23 @@ public final class CommonMethods {
 	}
 
 	/**
-	 * Insert a space after every 4 characters in a String.
-	 * Warning! Recursion!!!
-	 * This method takes the first four characters of some string, adds a space to the end of those 4 characters
-	 * and then appends it to the rest of the input string, minus those 4 beginning characters and the space.
+	 * Insert a space after every 4 characters in a String. Warning!
+	 * Recursion!!! This method takes the first four characters of some string,
+	 * adds a space to the end of those 4 characters and then appends it to the
+	 * rest of the input string, minus those 4 beginning characters and the
+	 * space.
 	 */
-	public final static String getStringWithSpacesEvery4Characters(final String stringWithoutSpaces) {
-		if(stringWithoutSpaces != null && stringWithoutSpaces.length() > 3)
-			return stringWithoutSpaces.substring(0, 4) + " " + getStringWithSpacesEvery4Characters(stringWithoutSpaces.substring(4));
+	public final static String getStringWithSpacesEvery4Characters(
+			final String stringWithoutSpaces) {
+		if (stringWithoutSpaces != null && stringWithoutSpaces.length() > 3)
+			return stringWithoutSpaces.substring(0, 4)
+					+ " "
+					+ getStringWithSpacesEvery4Characters(stringWithoutSpaces
+							.substring(4));
 		else
-			return stringWithoutSpaces;	
+			return stringWithoutSpaces;
 	}
-	
+
 	/**
 	 * Determines whether or not the current account's card type is a Cashback
 	 * Rewards card.
@@ -159,7 +172,8 @@ public final class CommonMethods {
 	 */
 	public final static boolean isCashbackCard(AccountDetails accountDetails) {
 
-		if (accountDetails.incentiveTypeCode.equalsIgnoreCase("CBB")) {
+		if (accountDetails.incentiveTypeCode
+				.equalsIgnoreCase(AccountCodes.INCENTIVE_TYPE_CBB)) {
 			return true;
 		} else {
 			return false;
@@ -174,7 +188,6 @@ public final class CommonMethods {
 	 * @return Formatted string with commas.
 	 */
 	public final static String insertCommas(String str) {
-		Log.e("what", str);
 		if (str.length() < 4) {
 			return str;
 		}
