63c0ff2e40f7bc9c440ee0021f009589b15e5ae5
Samuel Frank Smith <samfranksmith@gmail.com>
2013-01-23 13:24:23 -0600

    Implemented a way for BaseFragments to display the Discover logo instead of a String. This was to correct a defect on the Account Home which previously did not have the logo.

--- Phone/main/src/com/discover/mobile/LoggedInRoboActivity.java
+++ Phone/main/src/com/discover/mobile/LoggedInRoboActivity.java
@@ -19,91 +19,98 @@ import com.discover.mobile.logout.LogOutSuccessListener;
 import com.slidingmenu.lib.SlidingMenu;
 
 /**
- * This is used as the base activity for when the user has logged in.  Extending this will show the action bar
- * with the sliding bar as well as the title text and logout button.
+ * This is used as the base activity for when the user has logged in. Extending
+ * this will show the action bar with the sliding bar as well as the title text
+ * and logout button.
  * 
  * @author jthornton
- *
+ * 
  */
-public abstract class LoggedInRoboActivity extends BaseFragmentActivity{
-	
-	/**Pulled out variable for the fade of the sliding menu*/
+public abstract class LoggedInRoboActivity extends BaseFragmentActivity {
+
+	/** Pulled out variable for the fade of the sliding menu */
 	private static final float FADE = 0.35f;
-	/**Flag used to know when in the middle of a log out*/
+	/** Flag used to know when in the middle of a log out */
 	private static boolean pendingLogout = false;
-	
+
 	/**
 	 * Create the activity, set up the action bar and sliding menu
-	 * @param savedInstanceState - saved State of the activity
+	 * 
+	 * @param savedInstanceState
+	 *            - saved State of the activity
 	 */
 	@Override
-	public void onCreate(final Bundle savedInstanceState){
+	public void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		showActionBar();
 		setupSlidingMenu();
 	}
-	
-	 /**
-     *	Show the action bar with the custom layout
-     */
-    public void showActionBar(){
-    	setBehindContentView(R.layout.navigation_menu_frame);
-		
+
+	/**
+	 * Show the action bar with the custom layout
+	 */
+	public void showActionBar() {
+		setBehindContentView(R.layout.navigation_menu_frame);
+
 		final ActionBar actionBar = getSupportActionBar();
-		actionBar.setCustomView(getLayoutInflater().inflate(R.layout.action_bar_menu_layout, null));
+		actionBar.setCustomView(getLayoutInflater().inflate(
+				R.layout.action_bar_menu_layout, null));
 		actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-		
+
 		final TextView titleView = (TextView) findViewById(R.id.title_view);
 		final ImageView navigationToggle = (ImageView) findViewById(R.id.navigation_button);
 		final Button logout = (Button) findViewById(R.id.logout_button);
-		
+
 		navigationToggle.setVisibility(View.VISIBLE);
 		logout.setVisibility(View.VISIBLE);
 		navigationToggle.setVisibility(View.VISIBLE);
 		titleView.setVisibility(View.VISIBLE);
-		
+
 		titleView.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(final View v) {
 				updateStatusBarVisibility();
 			}
 		});
-		
-		navigationToggle.setOnClickListener(new OnClickListener(){
+
+		navigationToggle.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(final View v) {
 				toggle();
 			}
 		});
-		
-		logout.setOnClickListener(new OnClickListener(){
+
+		logout.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(final View v) {
 				logout();
 			}
-		});	
-    }
+		});
+	}
 
 	/**
-     * Log the user out
-     */
-    public void logout(){
-    	/** Used on pause to know when to set Globals isLoggedIn to false**/
-    	pendingLogout = true;
-    	
-    	
-		final AsyncCallback<Object> callback = 
-				GenericAsyncCallback.<Object>builder(this)
-				.showProgressDialog(getResources().getString(R.string.push_progress_get_title), 
-									getResources().getString(R.string.push_progress_registration_loading), 
-									true)
+	 * Log the user out
+	 */
+	public void logout() {
+		/** Used on pause to know when to set Globals isLoggedIn to false **/
+		pendingLogout = true;
+
+		final AsyncCallback<Object> callback = GenericAsyncCallback
+				.<Object> builder(this)
+				.showProgressDialog(
+						getResources().getString(
+								R.string.push_progress_get_title),
+						getResources().getString(
+								R.string.push_progress_registration_loading),
+						true)
 				.withSuccessListener(new LogOutSuccessListener(this))
-				.withErrorResponseHandler(new CardBaseErrorResponseHandler((ErrorHandlerUi) this))
+				.withErrorResponseHandler(
+						new CardBaseErrorResponseHandler((ErrorHandlerUi) this))
 				.build();
-	
+
 		new LogOutCall(this, callback).submit();
-	} 
-    
+	}
+
 	/**
 	 * Set up and style the sliding menu
 	 */
@@ -116,54 +123,70 @@ public abstract class LoggedInRoboActivity extends BaseFragmentActivity{
 		slidingMenu.setFadeDegree(FADE);
 		slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
 	}
-	
+
 	/**
-	 * Checks the shared pref to get the visibility for the status bar and 
-	 * sets the visibility
+	 * Checks the shared pref to get the visibility for the status bar and sets
+	 * the visibility
 	 */
-	public void setStatusBarVisbility(){
-		final FragmentTransaction ft = this.getSupportFragmentManager().beginTransaction();
+	public void setStatusBarVisbility() {
+		final FragmentTransaction ft = this.getSupportFragmentManager()
+				.beginTransaction();
 		final boolean statusBarVisitility = Globals.isStatusBarVisibility();
-		final Fragment statusBar = this.getSupportFragmentManager().findFragmentById(R.id.status_bar);
-		
+		final Fragment statusBar = this.getSupportFragmentManager()
+				.findFragmentById(R.id.status_bar);
+
 		/**
 		 * If its set to false hide the fragment, else show it.
 		 */
-		if (!statusBarVisitility){
+		if (!statusBarVisitility) {
 			ft.hide(statusBar);
-		}else {
+		} else {
 			ft.show(statusBar);
 		}
 		ft.commit();
 	}
-	
+
 	/**
-	 * Updates the shared preference for the status bar visibility and then calls 
-	 * setStatusBarVisibility to update the visibility
-	 * @param visible - boolean for setting the shared pref
+	 * Updates the shared preference for the status bar visibility and then
+	 * calls setStatusBarVisibility to update the visibility
+	 * 
+	 * @param visible
+	 *            - boolean for setting the shared pref
 	 */
-	public void updateStatusBarVisibility(){
-		final Fragment statusBar = this.getSupportFragmentManager().findFragmentById(R.id.status_bar);
+	public void updateStatusBarVisibility() {
+		final Fragment statusBar = this.getSupportFragmentManager()
+				.findFragmentById(R.id.status_bar);
 		boolean visible = true;
-		if (statusBar.isVisible()){
+		if (statusBar.isVisible()) {
 			visible = false;
-		}else if (statusBar.isHidden()){
-			visible=true;
+		} else if (statusBar.isHidden()) {
+			visible = true;
 		}
 		Globals.setStatusBarVisibility(visible);
-		
+
 		setStatusBarVisbility();
 	}
-	
+
 	@Override
 	public void onPause() {
 		super.onPause();
-		
-		//Clear all global variables after logout
-		if( pendingLogout ) {
+
+		// Clear all global variables after logout
+		if (pendingLogout) {
 			pendingLogout = false;
-			
+
 			Globals.setToDefaults();
 		}
-	}			
+	}
+
+	/**
+	 * Displays the Discover logo in the actionBar as opposed to the TextView.
+	 */
+	public void showActionBarLogo() {
+		final TextView titleView = (TextView) findViewById(R.id.title_view);
+		final ImageView titleImageView = (ImageView) findViewById(R.id.action_bar_discover_logo);
+		
+		titleView.setVisibility(View.GONE);
+		titleImageView.setVisibility(View.VISIBLE);
+	}
 }
