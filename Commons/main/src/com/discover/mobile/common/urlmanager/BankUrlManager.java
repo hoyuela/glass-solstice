e95035ae9ecf7644edfa217100fc983a0f23fda5
Henry Oyuela <hoyuela@solsticemobile.com>
2013-02-08 19:28:06 -0600

    Forgot to fix URL (Still commit for code freeze)

--- Commons/main/src/com/discover/mobile/common/urlmanager/BankUrlManager.java
+++ Commons/main/src/com/discover/mobile/common/urlmanager/BankUrlManager.java
@@ -13,11 +13,11 @@ import com.discover.mobile.common.net.json.bank.ReceivedUrl;
  * 
  */
 public class BankUrlManager {
-	//  private static final String BASE_URL = "https://beta.discoverbank.com";
+	  private static final String BASE_URL = "https://beta.discoverbank.com";
 	//	private static final String BASE_URL = "http://192.168.2.177:8008"; //Henry's Laptop
 	//	private static final String BASE_URL = "http://solsticebeta.com/Discover/DiscoverBank";
 	//	private static final String BASE_URL = "http://192.168.1.94:8008";
-		private static final String BASE_URL = "http://solsticebeta.com/Discover/Users/Henry/DiscoverBank";
+	//	private static final String BASE_URL = "http://solsticebeta.com/Discover/Users/Henry/DiscoverBank";
 	//private static final String BASE_URL = "http://192.168.1.70:8009"; //Jon's Laptop
 	//private static final String BASE_URL = "http://solsticebeta.com/Discover/Users/Jon/DiscoverBank"; //Jon Mock Service
 
