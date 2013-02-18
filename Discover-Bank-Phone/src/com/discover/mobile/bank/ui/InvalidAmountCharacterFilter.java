fca9e3f604048d01ca754eb6a3a2450d24177a91
Samuel Frank Smith <samfranksmith@gmail.com>
2013-02-18 13:22:38 -0600

    Previous commit workedgit status Here are some EditText filters I'm using for special characters and numbers.

---
+++ Discover-Bank-Phone/src/com/discover/mobile/bank/ui/InvalidAmountCharacterFilter.java
@@ -0,0 +1,30 @@
+package com.discover.mobile.bank.ui;
+
+import android.text.InputFilter;
+import android.text.Spanned;
+
+/**
+ * 
+ * @author Samuel Frank Smith
+ * 
+ */
+public class InvalidAmountCharacterFilter implements InputFilter {
+
+	char[] validCharacters = { '0', '1', '2', '3', '4', '5', '6', '7', '8',
+			'9', '.', ',' };
+
+	@Override
+	public CharSequence filter(final CharSequence source, final int start,
+			final int end, final Spanned dest, final int dstart, final int dend) {
+		if (end > start) {
+			for (int index = start; index < end; index++) {
+				if (!new String(validCharacters).contains(String.valueOf(source
+						.charAt(index)))) {
+					return "";
+				}
+			}
+		}
+		return null;
+	}
+
+}
