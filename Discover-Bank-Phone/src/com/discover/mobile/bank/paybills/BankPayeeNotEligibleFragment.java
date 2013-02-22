6fd8d283f94768fa17ff13edc09c9143fb25683e
Jon Thornton <jthornton@solstice-consulting.com>
2013-02-21 20:37:05 -0600

    US6157: Updated the landing screen with the open an account button

--- Discover-Bank-Phone/src/com/discover/mobile/bank/paybills/BankPayeeNotEligibleFragment.java
+++ Discover-Bank-Phone/src/com/discover/mobile/bank/paybills/BankPayeeNotEligibleFragment.java
@@ -3,9 +3,13 @@ package com.discover.mobile.bank.paybills;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.discover.mobile.bank.BankNavigator;
 import com.discover.mobile.bank.R;
+import com.discover.mobile.bank.services.BankUrlManager;
 import com.discover.mobile.common.BaseFragment;
 
 /**
@@ -25,7 +29,18 @@ public class BankPayeeNotEligibleFragment extends BaseFragment{
 	 */
 	@Override
 	public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState){
-		return inflater.inflate(R.layout.payee_no_eligible, null);
+		final View view = inflater.inflate(R.layout.payee_no_eligible, null);
+
+		final Button button = (Button) view.findViewById(R.id.openAccount);
+
+		button.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(final View v) {
+				BankNavigator.navigateToBrowser(BankUrlManager.getOpenAccountUrl());	
+			}
+		});
+
+		return view;
 	}
 
 	/**
