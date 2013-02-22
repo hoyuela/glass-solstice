061eb36c1e4b9b5ea61c1f3fa1d384cdf0ad58a2
AJ Leeds <aleeds@solstice-mobile.com>
2013-02-22 09:24:59 -0600

    DE2146 - Added external link icon to Manage external accounts.

--- Discover-Bank-Phone/src/com/discover/mobile/bank/transfer/BankTransferMoneySectionInfo.java
+++ Discover-Bank-Phone/src/com/discover/mobile/bank/transfer/BankTransferMoneySectionInfo.java
@@ -1,7 +1,13 @@
 package com.discover.mobile.bank.transfer;
 
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import com.discover.mobile.bank.BankNavigator;
 import com.discover.mobile.bank.R;
 import com.discover.mobile.bank.account.BankAccountSummaryFragment;
+import com.discover.mobile.bank.services.BankUrlManager;
+import com.discover.mobile.common.nav.section.ClickComponentInfo;
 import com.discover.mobile.common.nav.section.FragmentComponentInfo;
 import com.discover.mobile.common.nav.section.GroupComponentInfo;
 
@@ -14,9 +20,20 @@ public final class BankTransferMoneySectionInfo extends GroupComponentInfo {
 				R.string.sub_section_title_scheduled_transfers,
 				BankAccountSummaryFragment.class), new FragmentComponentInfo(
 				R.string.sub_section_title_transfer_history,
-				BankAccountSummaryFragment.class), new FragmentComponentInfo(
-				R.string.sub_section_title_manage_external_accounts,
-				BankAccountSummaryFragment.class));
+				BankAccountSummaryFragment.class), new ClickComponentInfo(
+				R.string.sub_section_title_manage_external_accounts,true,
+				externalLink(BankUrlManager.getOpenAccountUrl())));
+	}
+	
+	private static OnClickListener externalLink(final String url){
+
+		return new OnClickListener() {
+
+			@Override
+			public void onClick(final View v) {
+				BankNavigator.navigateToBrowser(url);
+			}
+		};
 	}
 
 }
