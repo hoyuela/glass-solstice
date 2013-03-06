1dbdd9f5d5696282c55ec3e254be4005425ae5d2
Henry Oyuela <hoyuela@solsticemobile.com>
2013-03-05 21:28:36 -0600

    ID: US6803 Name: Android: Update Logout Service Call
    ID: DE2531
    Name: No Errors Displayed for Incorrect SA Answer
    
    Tested:
    - Tested Logout on live server (Currently server is returning 500 need to follow-up with server team) Client still handles this gracefully
    - Tested Delete Payment (Worked once, then got 500 from server. Need to follow-up with server team as to why it is consistently failing)
    - Tested Strong Auth, tested with invalid answer and made sure inline error string is shown.

--- Discover-Bank-Phone/src/com/discover/mobile/bank/services/XHttpMethodOverrideValues.java
+++ Discover-Bank-Phone/src/com/discover/mobile/bank/services/XHttpMethodOverrideValues.java
@@ -1,5 +1,11 @@
 package com.discover.mobile.bank.services;
 
+/**
+ * Enumeration of methods used for the X-HTTP-Method-Override header in an HTTP request.
+ * 
+ * @author henryoyuela
+ *
+ */
 public enum XHttpMethodOverrideValues {
 	PUT,
 	DELETE
