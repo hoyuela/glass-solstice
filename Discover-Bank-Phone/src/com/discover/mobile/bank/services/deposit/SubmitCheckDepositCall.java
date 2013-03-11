e98db68863829dded63b9b56cbe68eb22a49c26d
Scott Seward <sseward@solstice-mobile.com>
2013-03-11 11:37:57 -0500

    US6991 : TA13793 - Create service call for check deposit. Created a POST service call for check deposit. Needs to be tested.

---
+++ Discover-Bank-Phone/src/com/discover/mobile/bank/services/deposit/SubmitCheckDepositCall.java
@@ -0,0 +1,63 @@
+package com.discover.mobile.bank.services.deposit;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
+import android.content.Context;
+
+import com.discover.mobile.bank.services.BankJsonResponseMappingNetworkServiceCall;
+import com.discover.mobile.bank.services.BankUrlManager;
+import com.discover.mobile.bank.services.payment.CreatePaymentDetail;
+import com.discover.mobile.common.callback.AsyncCallback;
+import com.discover.mobile.common.net.ServiceCallParams.PostCallParams;
+import com.discover.mobile.common.net.SimpleReferenceHandler;
+import com.discover.mobile.common.net.TypedReferenceHandler;
+import com.discover.mobile.common.net.error.bank.BankErrorResponseParser;
+
+/**
+ * Service call to submit a check deposit.
+ * Sends a DepositDetail to the server through a POST request and then receives a DepositDetail
+ * back from the server with a 201 Created response if it was successful.
+ * 
+ * @author scottseward
+ *
+ */
+public class SubmitCheckDepositCall extends BankJsonResponseMappingNetworkServiceCall<DepositDetail> {
+
+		/**Reference handler to allow the call to be back on the UI*/
+		private final SimpleReferenceHandler<DepositDetail> handler;
+		
+		public SubmitCheckDepositCall(final Context context, final AsyncCallback<DepositDetail> callback,
+				final CreatePaymentDetail modelClass) {
+			super(context, new PostCallParams(BankUrlManager.getUrl(BankUrlManager.DEPOSITS_URL_KEY)) {{ //$NON-NLS-1$
+				requiresSessionForRequest = true;
+				sendDeviceIdentifiers = true;
+
+				body = modelClass;
+				
+				errorResponseParser = BankErrorResponseParser.instance();
+			}},
+			DepositDetail.class);
+			handler = new SimpleReferenceHandler<DepositDetail>(callback);
+		}
+
+		@Override
+		protected TypedReferenceHandler<DepositDetail> getHandler() {
+			// TODO Auto-generated method stub
+			
+			
+			return handler;
+		}
+		
+		@Override
+		protected DepositDetail parseSuccessResponse(final int status, final Map<String,List<String>> headers, final InputStream body)
+				throws IOException {
+			final DepositDetail data = super.parseSuccessResponse(status, headers, body);
+			
+			
+			return data;
+		}
+
+	}
