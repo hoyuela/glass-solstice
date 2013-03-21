865fceda099271089dc0fb543521b36697a157e2
Henry Oyuela <hoyuela@solsticemobile.com>
2013-03-20 22:06:56 -0500

    DE3226 - Joint Owner Payments - Display and Deletion (QC# 90378) 3226 DE3220 - Incorrect Text on Schedule a Payment Screen when no payees are set up (Android) DE3227 - Incorrect Select Account Field in Schedule Payments (Android and iOS) (QC# 90460)
    Defects:
    - Verified that delete and edit is NOT displayed in Payment Detail Page if a Scheduled Payment has jointPayment set to true
    - Verified that delete and edit is displayed in Payment Detail Page if a Scheduled Payment has jointPayment set to false
    - Verified nickname is displayed correctly in review payments
    - Verified nickname is displayed correctly in Payment details
    - Verified Payment detail works correctly on orientation change for both scheduled payment that is joint vs not being joint
    - Verified correct text is displayed for Managed Payees - Select Payee when no payees
    - Verified correct text is displayed for Pay Bill - Select Payee when no payees
    - Verified only Checking and MMA accounts are allowed to be selected from scheduled payment page

--- Discover-Bank-Phone/src/com/discover/mobile/bank/framework/BankUser.java
+++ Discover-Bank-Phone/src/com/discover/mobile/bank/framework/BankUser.java
@@ -1,6 +1,7 @@
 package com.discover.mobile.bank.framework;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 import com.discover.mobile.bank.deposit.CheckDepositCaptureActivity;
 import com.discover.mobile.bank.services.BankUrlManager;
@@ -68,6 +69,23 @@ public final class BankUser extends CacheManager implements Serializable {
 		return accountList;
 	}
 
+	/**
+	 * @return Returns reference to list of accounts that are capable of scheduled payments
+	 */
+	public AccountList getPaymentCapableAccounts() {
+		final AccountList newList = new AccountList();
+		newList.accounts = new ArrayList<Account>();
+		
+		if( this.hasAccounts() ) {
+			for(final Account account : accountList.accounts) {
+				if( account.canSchedulePayment() ) {
+					newList.accounts.add(account);
+				}
+			}
+		}
+		return newList;
+	}
+	
 	/**
 	 * Returns the {@code Account} for a given Account id.
 	 * 
