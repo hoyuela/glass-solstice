73b4a9af2c2b3405a631858167b7cc6f2ddc5711
Henry Oyuela <hoyuela@solsticemobile.com>
2013-04-19 21:24:21 -0500

    ID: US7225 - Android: SSO: Bank Login Updates
    Tested:
    - Verified can login into Bank
    - Verified Help Icon asset is correct in all pages
    - Verified Catch All modal does not include feedback link

--- Discover-Card-Phone/src/com/discover/mobile/card/navigation/StatusBarFragment.java
+++ Discover-Card-Phone/src/com/discover/mobile/card/navigation/StatusBarFragment.java
@@ -40,7 +40,7 @@ public class StatusBarFragment extends Fragment {
 	public View onCreateView(final LayoutInflater inflater,
 			final ViewGroup container, final Bundle savedInstanceState) {
 
-		final View view = inflater.inflate(R.layout.status_bar_layout,
+		final View view = inflater.inflate(R.layout.card_status_bar_layout,
 				container);
 		final CardShareDataStore cardShareDataStoreObj = CardShareDataStore
 				.getInstance(stsActivity);
