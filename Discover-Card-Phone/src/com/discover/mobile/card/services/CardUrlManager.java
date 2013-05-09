66a717de8b9830af0a4a0767b870e8ebb872e444
AJ Leeds <aleeds@solstice-mobile.com>
2013-05-08 19:56:11 -0500

    US7225 - Updated the urls for a prod build

--- Discover-Card-Phone/src/com/discover/mobile/card/services/CardUrlManager.java
+++ Discover-Card-Phone/src/com/discover/mobile/card/services/CardUrlManager.java
@@ -9,7 +9,7 @@ package com.discover.mobile.card.services;
 
 public class CardUrlManager {
 
-	private static final String BASE_URL = "https://asys.mapi.discovercard.com";
+	private static final String BASE_URL = "https://www.discovercard.com";
 
 	/**
 	 * Pre Auth URL
