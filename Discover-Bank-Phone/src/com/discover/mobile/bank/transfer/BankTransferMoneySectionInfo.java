2e70eefe5f7f8de095ee213dfeafdbec20d49557
Scott Seward <sseward@solstice-mobile.com>
2013-05-14 16:39:15 -0500

    DE5308 - Updated BankUser to support cacheing of external accounts.

--- Discover-Bank-Phone/src/com/discover/mobile/bank/transfer/BankTransferMoneySectionInfo.java
+++ Discover-Bank-Phone/src/com/discover/mobile/bank/transfer/BankTransferMoneySectionInfo.java
@@ -2,6 +2,7 @@ package com.discover.mobile.bank.transfer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 
