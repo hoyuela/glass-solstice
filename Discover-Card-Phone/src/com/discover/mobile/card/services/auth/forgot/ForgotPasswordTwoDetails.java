4635c315dd20f82551a6ad1c9b2c377bde3377c4
AJ Leeds <aleeds@solstice-mobile.com>
2013-05-20 13:12:39 -0500

    US7225 - More code merging from card

--- Discover-Card-Phone/src/com/discover/mobile/card/services/auth/forgot/ForgotPasswordTwoDetails.java
+++ Discover-Card-Phone/src/com/discover/mobile/card/services/auth/forgot/ForgotPasswordTwoDetails.java
@@ -2,17 +2,20 @@ package com.discover.mobile.card.services.auth.forgot;
 
 import java.io.Serializable;
 
-import com.discover.mobile.card.services.auth.registration.AccountInformationDetails;
 import com.discover.mobile.common.Struct;
 
+import com.discover.mobile.card.services.auth.registration.AccountInformationDetails;
+
 @Struct
-public class ForgotPasswordTwoDetails extends AccountInformationDetails implements Serializable {
+public class ForgotPasswordTwoDetails extends AccountInformationDetails
+        implements Serializable {
 
-	private static final long serialVersionUID = 5094796037192037185L;
-	
-	// TODO combine with CreateLoginDetails like the original ForgotPasswordDetails
+    private static final long serialVersionUID = 5094796037192037185L;
 
-	public String password;
-	public String passwordConfirm;
-	
+    // TODO combine with CreateLoginDetails like the original
+    // ForgotPasswordDetails
+
+    public String password;
+    public String passwordConfirm;
+
 }
