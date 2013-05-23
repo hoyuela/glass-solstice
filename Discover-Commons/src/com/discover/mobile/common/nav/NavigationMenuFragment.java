7d3dfc2fbb673912de4f5772de02455538d980ca
AJ Leeds <aleeds@solstice-mobile.com>
2013-05-23 14:53:31 -0500

    US7225 - More code merging from card - fixing the highlight for the home menu entry

--- Discover-Commons/src/com/discover/mobile/common/nav/NavigationMenuFragment.java
+++ Discover-Commons/src/com/discover/mobile/common/nav/NavigationMenuFragment.java
@@ -20,19 +20,22 @@ public abstract class NavigationMenuFragment extends RoboSherlockListFragment {
 	public void setItemSelected(final int group, final int subSection){
 		if(NavigationIndex.getMainIndex()>=0)
 		{
-		if(getListView() != null && 
-				getListView().getAdapter().getItem(NavigationIndex.getMainIndex()) instanceof GroupNavigationItem){
-			final GroupNavigationItem currentGroup = 
-					(GroupNavigationItem)getListView().getAdapter().getItem(NavigationIndex.getMainIndex());
-			currentGroup.collapse();
-		}
-		final int maxIndex = getListView().getAdapter().getCount();
-		if(group < maxIndex && getListView().getAdapter().getItem(group) instanceof GroupNavigationItem){
-			final GroupNavigationItem newGroup = (GroupNavigationItem)getListView().getAdapter().getItem(group);
-			newGroup.expand();
-			NavigationIndex.setIndex(group);
-		}
-		NavigationIndex.setSubIndex(subSection+group);
+			if (getListView().getAdapter().getItem(NavigationIndex.getMainIndex()) instanceof GroupNavigationItem) {
+				final GroupNavigationItem currentGroup = (GroupNavigationItem) getListView().getAdapter().getItem(NavigationIndex.getMainIndex());
+				currentGroup.collapse();
+			}
+
+			final int maxIndex = getListView().getAdapter().getCount();
+
+			if (group < maxIndex && getListView().getAdapter().getItem(group) instanceof GroupNavigationItem) {
+				final GroupNavigationItem newGroup = (GroupNavigationItem) getListView().getAdapter().getItem(group);
+				newGroup.expand();
+				NavigationIndex.setIndex(group);
+			} else {
+				NavigationIndex.setIndex(group);
+			}
+
+			NavigationIndex.setSubIndex(subSection + group);
 		}
 	}
 	
