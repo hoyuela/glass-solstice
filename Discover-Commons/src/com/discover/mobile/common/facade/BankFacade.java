9356eccb513b95aab5a4e701406f18d8b7424d0a
Allie <acurry@solstice-mobile.com>
2013-05-28 12:00:06 -0500

    Added Bank Facade so that Privacy & Terms can be launched from anywhere in the app.

---
+++ Discover-Commons/src/com/discover/mobile/common/facade/BankFacade.java
@@ -0,0 +1,15 @@
+package com.discover.mobile.common.facade;
+
+/**
+ * A facade for assisting with general bank activities.
+ * 
+ * @author acurry
+ */
+public interface BankFacade {
+
+	/**
+	 * Navigates to Card Privacy and Terms.
+	 */
+	public void navToCardPrivacyTerms();
+	
+}
