310a80c683310c5af00b9c0aecc5c96686c18169
Jon Thornton <jthornton@solstice-consulting.com>
2013-05-29 08:33:00 -0500

    DE5608: Fixed the preauth modal so that it does not keep showing itself.

--- Discover-Bank-Phone/src/com/discover/mobile/bank/login/PreAuthCallHelper.java
+++ Discover-Bank-Phone/src/com/discover/mobile/bank/login/PreAuthCallHelper.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 
 import com.discover.mobile.bank.R;
+import com.discover.mobile.common.DiscoverModalManager;
 import com.discover.mobile.common.error.ErrorHandlerUi;
 import com.discover.mobile.common.ui.modals.ModalAlertWithOneButton;
 import com.discover.mobile.common.ui.modals.ModalAlertWithTwoButtons;
@@ -31,26 +32,26 @@ import com.discover.mobile.common.ui.modals.ModalDefaultTwoButtonBottomView;
  *
  */
 public final class PreAuthCallHelper  {
-	
-	
+
+
 	protected static final String DATETIME_KEY = "com.discover.mobile.optionalupdatedate";
 
 	protected static final int NO_DATE = 0;
 	protected static final int OPTIONAL_UPGRADE = 1;
 	protected static final int FORCED_UPGRADE = 2;
 	protected static final int DOES_NOT_EXIST = 0;
-	
+
 	protected static final long DAY_IN_MILLISECONDS = 86400000;
 	protected static final int OPTIONAL_UPGRADE_DAYS = 30;
 	protected static final String PACKAGE_NAME = "com.discoverfinancial.mobile";
-	
+
 	protected static final String PREFS_FILE = "UpdatePreferences";
 
 	private PreAuthCallHelper() {
 		// No need to instantiate or extend this class since all methods are static.
 		throw new AssertionError();
 	}
-	
+
 	/**
 	 * Shows the optional upgrade message to the user.
 	 * This message is cancelable with no consequences. The user also has the option to press
@@ -64,29 +65,29 @@ public final class PreAuthCallHelper  {
 
 		final ModalDefaultTopView titleAndContentForDialog = new ModalDefaultTopView(context, null);
 		final ModalDefaultTwoButtonBottomView twoButtonBottomView = new ModalDefaultTwoButtonBottomView(context, null);
-		
+
 		titleAndContentForDialog.hideNeedHelpFooter();
 		titleAndContentForDialog.setTitle(R.string.option_upgrade_dialog_title);
 		titleAndContentForDialog.setContent(R.string.optional_upgrade_dialog_body);
-		
+
 		twoButtonBottomView.setOkButtonText(R.string.upgrade_dialog_button_text);
 		twoButtonBottomView.setCancelButtonText(R.string.no_thanks);
-				
+
 		final ModalAlertWithTwoButtons optionalUpgradeDialog = 
 				new ModalAlertWithTwoButtons(context, titleAndContentForDialog, twoButtonBottomView);
-		
+
 		twoButtonBottomView.getOkButton().setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(final View v) { upgrade(context); }
 		});
-		
+
 		twoButtonBottomView.getCancelButton().setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(final View v) { optionalUpgradeDialog.dismiss(); }
 		});
 		errorHandlerUi.showCustomAlert(optionalUpgradeDialog);
 	}
-	
+
 	/**
 	 * showForcedUpgradeAlertDialog()
 	 * This method, when called, shows an alert dialog with the forced upgrade text.
@@ -98,17 +99,17 @@ public final class PreAuthCallHelper  {
 		final Context context = errorHandlerUi.getContext();
 		final ModalDefaultTopView titleAndContentForDialog = new ModalDefaultTopView(context, null);
 		final ModalDefaultOneButtonBottomView singleButtonBottomView = new ModalDefaultOneButtonBottomView(context, null);
-		
+
 		titleAndContentForDialog.hideNeedHelpFooter();
 		titleAndContentForDialog.setTitle(R.string.forced_upgrade_dialog_title);
 		titleAndContentForDialog.setContent(R.string.forced_upgrade_dialog_body);
 		titleAndContentForDialog.showErrorIcon(true);
-		
+
 		singleButtonBottomView.setButtonText(R.string.upgrade_dialog_button_text);
-				
+
 		final ModalAlertWithOneButton optionalUpgradeDialog = 
 				new ModalAlertWithOneButton(context, titleAndContentForDialog, singleButtonBottomView);
-		
+
 		singleButtonBottomView.getButton().setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(final View v) { upgrade(context); }
@@ -119,10 +120,12 @@ public final class PreAuthCallHelper  {
 				android.os.Process.killProcess(android.os.Process.myPid());
 			}
 		});	
-		
-		errorHandlerUi.showCustomAlert(optionalUpgradeDialog);
+
+		if(!DiscoverModalManager.hasActiveModal()){
+			errorHandlerUi.showCustomAlert(optionalUpgradeDialog);
+		}
 	}
-	
+
 	/**
 	 * shouldPresentOptionalUpdate()
 	 * @param updateDescription The upgrade message to be displayed in the dialog.
@@ -131,27 +134,27 @@ public final class PreAuthCallHelper  {
 	protected static final boolean shouldPresentOptionalUpdate(final Activity activity, final String updateDescription) {
 		if(updateDescription != null) {
 			final SharedPreferences prefs = activity.getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE);
-			
+
 			final long savedDate = prefs.getLong(DATETIME_KEY, DOES_NOT_EXIST);
-			
+
 			if (savedDate == DOES_NOT_EXIST) {
 				final SharedPreferences.Editor editor = prefs.edit();
 				editor.putLong(DATETIME_KEY, new Date().getTime());
 				editor.commit();
 				return true;
 			}
-			
+
 			final long currentDate = new Date().getTime();
 			final long daysDifference = (currentDate - savedDate)/DAY_IN_MILLISECONDS;
-			
+
 			if(daysDifference >= OPTIONAL_UPGRADE_DAYS) {
 				updateDateInPrefs(activity);
 				return true;
 			}
-			
+
 			return false;
 		}
-		
+
 		return false;
 	}
 
@@ -162,7 +165,7 @@ public final class PreAuthCallHelper  {
 	protected static void upgrade(final Context context) {
 		final Uri marketUri = Uri.parse("market://details?id=" + PACKAGE_NAME);
 		final Intent androidMarketplaceIntent = new Intent(Intent.ACTION_VIEW, marketUri);
-		context.startActivity(androidMarketplaceIntent);
+		((LoginActivity)context).startActivityNoReset(androidMarketplaceIntent);
 	}
 
 	/**
@@ -172,10 +175,10 @@ public final class PreAuthCallHelper  {
 	 */
 	protected static void updateDateInPrefs(final Context context) {
 		final SharedPreferences prefs = context.getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE);
-		
+
 		final SharedPreferences.Editor editor = prefs.edit();
 		editor.putLong(DATETIME_KEY, new Date().getTime());
 		editor.commit();
 	}
-	
+
 }
