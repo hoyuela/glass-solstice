1750199bef698bf0a11246911af855cbcfb5f9d4
AJ Leeds <aleeds@solstice-mobile.com>
2013-05-31 15:10:51 -0500

    US7225 - 13.3 code merging from card 5/31 - Sliding Menu Update

--- SlidingMenuActivity/src/com/slidingmenu/lib/app/SlidingActivity.java
+++ SlidingMenuActivity/src/com/slidingmenu/lib/app/SlidingActivity.java
@@ -1,14 +1,14 @@
 package com.slidingmenu.lib.app;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 
-import com.actionbarsherlock.app.SherlockActivity;
 import com.slidingmenu.lib.SlidingMenu;
 
-public class SlidingActivity extends SherlockActivity implements SlidingActivityBase {
+public class SlidingActivity extends Activity implements SlidingActivityBase {
 
 	private SlidingActivityHelper mHelper;
 
@@ -16,7 +16,7 @@ public class SlidingActivity extends SherlockActivity implements SlidingActivity
 	 * @see android.app.Activity#onCreate(android.os.Bundle)
 	 */
 	@Override
-	public void onCreate(final Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		mHelper = new SlidingActivityHelper(this);
 		mHelper.onCreate(savedInstanceState);
@@ -26,7 +26,7 @@ public class SlidingActivity extends SherlockActivity implements SlidingActivity
 	 * @see android.app.Activity#onPostCreate(android.os.Bundle)
 	 */
 	@Override
-	public void onPostCreate(final Bundle savedInstanceState) {
+	public void onPostCreate(Bundle savedInstanceState) {
 		super.onPostCreate(savedInstanceState);
 		mHelper.onPostCreate(savedInstanceState);
 	}
@@ -35,8 +35,8 @@ public class SlidingActivity extends SherlockActivity implements SlidingActivity
 	 * @see android.app.Activity#findViewById(int)
 	 */
 	@Override
-	public View findViewById(final int id) {
-		final View v = super.findViewById(id);
+	public View findViewById(int id) {
+		View v = super.findViewById(id);
 		if (v != null)
 			return v;
 		return mHelper.findViewById(id);
@@ -46,7 +46,7 @@ public class SlidingActivity extends SherlockActivity implements SlidingActivity
 	 * @see android.app.Activity#onSaveInstanceState(android.os.Bundle)
 	 */
 	@Override
-	protected void onSaveInstanceState(final Bundle outState) {
+	protected void onSaveInstanceState(Bundle outState) {
 		super.onSaveInstanceState(outState);
 		mHelper.onSaveInstanceState(outState);
 	}
@@ -55,7 +55,7 @@ public class SlidingActivity extends SherlockActivity implements SlidingActivity
 	 * @see android.app.Activity#setContentView(int)
 	 */
 	@Override
-	public void setContentView(final int id) {
+	public void setContentView(int id) {
 		setContentView(getLayoutInflater().inflate(id, null));
 	}
 
@@ -63,7 +63,7 @@ public class SlidingActivity extends SherlockActivity implements SlidingActivity
 	 * @see android.app.Activity#setContentView(android.view.View)
 	 */
 	@Override
-	public void setContentView(final View v) {
+	public void setContentView(View v) {
 		setContentView(v, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
 	}
 
@@ -71,7 +71,7 @@ public class SlidingActivity extends SherlockActivity implements SlidingActivity
 	 * @see android.app.Activity#setContentView(android.view.View, android.view.ViewGroup.LayoutParams)
 	 */
 	@Override
-	public void setContentView(final View v, final LayoutParams params) {
+	public void setContentView(View v, LayoutParams params) {
 		super.setContentView(v, params);
 		mHelper.registerAboveContentView(v, params);
 	}
@@ -79,21 +79,21 @@ public class SlidingActivity extends SherlockActivity implements SlidingActivity
 	/* (non-Javadoc)
 	 * @see com.slidingmenu.lib.app.SlidingActivityBase#setBehindContentView(int)
 	 */
-	public void setBehindContentView(final int id) {
+	public void setBehindContentView(int id) {
 		setBehindContentView(getLayoutInflater().inflate(id, null));
 	}
 
 	/* (non-Javadoc)
 	 * @see com.slidingmenu.lib.app.SlidingActivityBase#setBehindContentView(android.view.View)
 	 */
-	public void setBehindContentView(final View v) {
+	public void setBehindContentView(View v) {
 		setBehindContentView(v, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
 	}
 
 	/* (non-Javadoc)
 	 * @see com.slidingmenu.lib.app.SlidingActivityBase#setBehindContentView(android.view.View, android.view.ViewGroup.LayoutParams)
 	 */
-	public void setBehindContentView(final View v, final LayoutParams params) {
+	public void setBehindContentView(View v, LayoutParams params) {
 		mHelper.setBehindContentView(v, params);
 	}
 
@@ -114,21 +114,28 @@ public class SlidingActivity extends SherlockActivity implements SlidingActivity
 	/* (non-Javadoc)
 	 * @see com.slidingmenu.lib.app.SlidingActivityBase#showAbove()
 	 */
-	public void showAbove() {
-		mHelper.showAbove();
+	public void showContent() {
+		mHelper.showContent();
 	}
 
 	/* (non-Javadoc)
 	 * @see com.slidingmenu.lib.app.SlidingActivityBase#showBehind()
 	 */
-	public void showBehind() {
-		mHelper.showBehind();
+	public void showMenu() {
+		mHelper.showMenu();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.slidingmenu.lib.app.SlidingActivityBase#showSecondaryMenu()
+	 */
+	public void showSecondaryMenu() {
+		mHelper.showSecondaryMenu();
 	}
 
 	/* (non-Javadoc)
 	 * @see com.slidingmenu.lib.app.SlidingActivityBase#setSlidingActionBarEnabled(boolean)
 	 */
-	public void setSlidingActionBarEnabled(final boolean b) {
+	public void setSlidingActionBarEnabled(boolean b) {
 		mHelper.setSlidingActionBarEnabled(b);
 	}
 
@@ -136,8 +143,8 @@ public class SlidingActivity extends SherlockActivity implements SlidingActivity
 	 * @see android.app.Activity#onKeyUp(int, android.view.KeyEvent)
 	 */
 	@Override
-	public boolean onKeyUp(final int keyCode, final KeyEvent event) {
-		final boolean b = mHelper.onKeyUp(keyCode, event);
+	public boolean onKeyUp(int keyCode, KeyEvent event) {
+		boolean b = mHelper.onKeyUp(keyCode, event);
 		if (b) return b;
 		return super.onKeyUp(keyCode, event);
 	}
