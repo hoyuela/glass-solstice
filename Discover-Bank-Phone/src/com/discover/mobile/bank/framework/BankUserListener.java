f7fead3367848673a6e8112193baf220f6198386
Henry Oyuela <hoyuela@solsticemobile.com>
2013-06-05 21:42:19 -0500

    no message

--- Discover-Bank-Phone/src/com/discover/mobile/bank/framework/BankUserListener.java
+++ Discover-Bank-Phone/src/com/discover/mobile/bank/framework/BankUserListener.java
@@ -1,10 +1,11 @@
-package com.discover.mobile.bank;
+package com.discover.mobile.bank.framework;
 
 import java.util.List;
 
-import com.discover.mobile.bank.framework.BankUser;
 import com.discover.mobile.bank.services.account.Account;
 
 public interface BankUserListener {
 	public void onAccountsUpdate(BankUser sender, List<Account> accounts);
+
+	public void onCurrentAccountUpdate(BankUser sender, Account account);
 }
--- Discover-Bank-Phone/src/com/discover/mobile/bank/framework/BankUserVisitors.java
+++ Discover-Bank-Phone/src/com/discover/mobile/bank/framework/BankUserVisitors.java
@@ -1,19 +1,26 @@
-package com.discover.mobile.bank;
+package com.discover.mobile.bank.framework;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import com.discover.mobile.bank.framework.BankUser;
 import com.discover.mobile.bank.services.account.Account;
 import com.discover.mobile.bank.services.account.AccountList;
 
-final public class BankUserVisitors {
+final class BankUserVisitors {
 	static public void visitAccountChangeListeners(final BankUser sender, final List<BankUserListener> listeners, final AccountList acctList) {
 		if (sender != null && listeners != null) {
 			final List<Account> accounts = (acctList != null && acctList.accounts != null) ? acctList.accounts : new ArrayList<Account>();
 
 			for (final BankUserListener listener : listeners) {
 				listener.onAccountsUpdate(sender, accounts);
+			}
+		}
+	}
+
+	static public void visitCurrentAccountChangeListeners(final BankUser sender, final List<BankUserListener> listeners, final Account account) {
+		if (sender != null && listeners != null) {
+			for (final BankUserListener listener : listeners) {
+				listener.onCurrentAccountUpdate(sender, account);
 			}
 		}
 	}
