b82b0b9dcb587f95a8636f52232e51fefcc75b6e
Jon Thornton <jthornton@solstice-consulting.com>
2013-08-16 11:29:26 -0500

    US7225: Code Merge 8/16/2013 - Discover Bank

--- Discover-Bank-Phone/src/com/discover/mobile/bank/ui/widgets/BankLayoutFooter.java
+++ Discover-Bank-Phone/src/com/discover/mobile/bank/ui/widgets/BankLayoutFooter.java
@@ -11,9 +11,12 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.discover.mobile.common.facade.FacadeFactory;
+
 import com.discover.mobile.bank.R;
 import com.discover.mobile.bank.framework.BankConductor;
 import com.discover.mobile.bank.help.PrivacyTermsType;
+import com.discover.mobile.bank.login.LoginActivity;
 import com.discover.mobile.bank.util.BankNeedHelpFooter;
 
 public class BankLayoutFooter extends LinearLayout implements OnClickListener {
@@ -23,6 +26,8 @@ public class BankLayoutFooter extends LinearLayout implements OnClickListener {
 	private TextView copyRight;
 	private BankNeedHelpFooter helpFooter;
 	private String provideFeedbackUrl;
+	//16/Aug/2013---Observation Fixed
+	private Context context ;
 
 	/**Set to true when the footer needs to send the user to card pages*/
 	private boolean cardMode = false;
@@ -65,14 +70,14 @@ public class BankLayoutFooter extends LinearLayout implements OnClickListener {
 		for (int i = 0; i < N; ++i) {
 			final int attr = a.getIndex(i);
 			switch (attr) {
-				case R.styleable.com_discover_mobile_bank_ui_widgets_BankLayoutFooter_footerType:
-					final int footerType = a.getInt(attr,FooterType.PRIVACY_TERMS);
-					this.setFooterType(footerType);
-					break;
-				case R.styleable.com_discover_mobile_bank_ui_widgets_BankLayoutFooter_helpNumber:
-					final String helpNumber = a.getString(attr);
-					helpFooter.setToDialNumberOnClick(helpNumber);
-					break;
+			case R.styleable.com_discover_mobile_bank_ui_widgets_BankLayoutFooter_footerType:
+				final int footerType = a.getInt(attr,FooterType.PRIVACY_TERMS);
+				this.setFooterType(footerType);
+				break;
+			case R.styleable.com_discover_mobile_bank_ui_widgets_BankLayoutFooter_helpNumber:
+				final String helpNumber = a.getString(attr);
+				helpFooter.setToDialNumberOnClick(helpNumber);
+				break;
 				case R.styleable.com_discover_mobile_bank_ui_widgets_BankLayoutFooter_provideFeedbackUrl:
 					provideFeedbackUrl = a.getString(attr);
 					break;
@@ -93,6 +98,8 @@ public class BankLayoutFooter extends LinearLayout implements OnClickListener {
 	private void initialize(final Context context) {
 		this.addView(getInflatedLayout(context));
 
+		//16/Aug/2013---Observation Fixed
+		this.context = context ;
 		/** Load UI Widgets */
 		divider = (TextView) this.findViewById(R.id.divider);
 		provideFeedback = (Button) this.findViewById(R.id.provide_feedback_button);
@@ -165,7 +172,9 @@ public class BankLayoutFooter extends LinearLayout implements OnClickListener {
 			if(!cardMode){
 				BankConductor.navigateToPrivacyTerms(PrivacyTermsType.LandingPage);
 			}else{
-				BankConductor.navigateToCardPrivacyAndTermsLanding();
+			    //16/Aug/2013---Observation Fixed
+				//BankConductor.navigateToCardPrivacyAndTermsLanding();
+			    FacadeFactory.getCardFacade().navToPrivacyTerms(context);
 			}
 		}
 	}
