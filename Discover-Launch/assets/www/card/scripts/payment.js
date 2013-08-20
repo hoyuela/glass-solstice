b90eac4be9737bde7c3140bb55ab6e1240489678
Jon Thornton <jthornton@solstice-consulting.com>
2013-08-19 19:52:07 -0500

    US7225: Code Merge 8/19/2013 - Discover Launch

--- Discover-Launch/assets/www/card/scripts/payment.js
+++ Discover-Launch/assets/www/card/scripts/payment.js
@@ -166,6 +166,7 @@ dfs.crd.pymt.populatePaymentSummaryPageDivs = function(payDataObj, pageId){
 			if(dfs.crd.pymt.afterMakePayFlag){
 				inLineError.css("display", "block");
 				inLineError.html(dfs.crd.pymt.messageAftrmakePay);
+				dfs.crd.pymt.afterMakePayFlag = false;
 			}
 		}
 
@@ -737,6 +738,7 @@ function paymentStep1Load()
 //						minPayStepOne.parent(".wrapperSpan").addClass('errormsg');
 						$("#minpaystepone_other").val('');
 						$('#minpaystepone_other').attr("placeholder", '0.00');
+						dfs.crd.sct.pageErrorTrackingSiteCat(errorCodeMap["1207"]);
 						return;
 					}
 					else if (!isEmpty(payAmount) && !dfs.crd.pymt.checkCurrency(payAmount)) 
@@ -1562,6 +1564,7 @@ dfs.crd.pymt.bankSelected = function()
 						$.mobile.activePage.find("#makePaymentOneContinue")
 						.attr("disabled", "true").parent().addClass(
 						"ui-disabled");
+						dfs.crd.sct.pageErrorTrackingSiteCat(errorCodeMap["NoDates_for_Bank"]);
 						return false;
 						// otherwise enable button & remove error
 					}else {          
@@ -1828,23 +1831,26 @@ dfs.crd.pymt.continuePaymentStep1ToStep2 = function()
 						commonErrorMap1.html(errorCodeMap["Update_HighLighted"]);
 						var errorMessage = errorCodeMap["1209"];
 						var payDetail = [];
+						var parseContentText = "";
 						var pendingPaymentsList = getDataFromCache("PENDINGPAYMENTS");
 						if(!isEmpty(pendingPaymentsList) &&  (pendingPaymentsList.pendingPayments.length == 1)){
 							var errorMessage = errorCodeMap["1208"];
 							payDetail["currentBalance"] = value;
 							parseContentText = parseContent(errorMessage, payDetail);
+							dfs.crd.sct.pageErrorTrackingSiteCat(parseContentText);
 						}else{
 							payDetail["outstandingBalance"] = outStandingBalance;
-							var parseContentText = parseContent(errorMessage, payDetail);
+							parseContentText = parseContent(errorMessage, payDetail);
+							dfs.crd.sct.pageErrorTrackingSiteCat(parseContentText);
 						}
 						
 
 						
-						if(!isEmpty(pendingPaymentsList) &&  (pendingPaymentsList.length == 1)){
+				/*		if(!isEmpty(pendingPaymentsList) &&  (pendingPaymentsList.length == 1)){
 							var errorMessage = errorCodeMap["1208"];
 							payDetail["currentBalance"] = value;
 							parseContentText = parseContent(errorMessage, payDetail);
-						}
+						}*/
 						$("#errorPaymentAmountExceed").text(
 								parseContent(parseContentText, curntValue));
 						$("#minpaystepone_other").parent(".wrapperSpan").addClass("inputOnError");
@@ -2128,6 +2134,9 @@ dfs.crd.pymt.getPaymentVerifErrorHandler = function(jqXHR){
 		hideSpinner();
 		cpEvent.preventDefault();
 		var code = getResponseStatusCode(jqXHR);
+		if(code == "1203"){
+			dfs.crd.sct.bankAccountDeletedModal();
+		}
 		errorHandler(code, "", "paymentStep2");
 	}catch (err) {
 		showSysException(err);
@@ -2626,6 +2635,18 @@ dfs.crd.pymt.payStep3ConfirmErrorHandler = function(jqXHR){
 			var errorMessage = errorCodeMap["1257"];
 			if(projectBeyondCard)
 				errorMessage = errorCodeMap["1257_PB"];
+			dfs.crd.sct.pageErrorTrackingSiteCat(errorMessage);
+			if (!isEmpty(errorMessage))
+				errorHandler(code, errorMessage,
+				"paymentStep1");
+			else
+				errorHandler(code, "", "paymentStep1");
+
+			break;
+		case "1203":
+			var errorMsgData = getResponsErrorData(jqXHR);
+			var errorMessage = errorCodeMap["1203"];
+			dfs.crd.sct.bankAccountDeletedModal();
 			if (!isEmpty(errorMessage))
 				errorHandler(code, errorMessage,
 				"paymentStep1");
@@ -3045,6 +3066,9 @@ try{
 		hideSpinner();
 		cpEvent.preventDefault();
 		var code = getResponseStatusCode(jqXHR);
+		if(code == "1203"){
+			dfs.crd.sct.bankAccountDeletedModal();
+		}
 		errorHandler(code, "", "pendingPayment");
 	} catch (err) {
 		showSysException(err);
@@ -3292,6 +3316,9 @@ dfs.crd.pymt.populateCancelPayment1ErrorDivs = function(jqXHR){
 			cpEvent.preventDefault();
 			navigation("../payments/pendingPayments");
 		}else{
+			if(code == "1203"){
+				dfs.crd.sct.bankAccountDeletedModal();
+			}
 			cpEvent.preventDefault();
 			errorHandler(code, "", "cancelPayment1");
 		}
@@ -3381,11 +3408,15 @@ dfs.crd.pymt.populatePostConfirmCancelErrorDivs = function(jqXHR){
 				navigation("../payments/pendingPayments");
 		}else if(code == "1256"){
 			var errorMessage = errorCodeMap["1256"];
-			if(projectBeyondCard){
+						if(projectBeyondCard){
 				errorMessage = errorCodeMap["1256_PB"];
 			}
+			dfs.crd.sct.pageErrorTrackingSiteCat(errorMessage);
 			errorHandler(code,errorMessage, "confirmCancelPayment");
 		}else{
+			if(code == "1203"){
+				dfs.crd.sct.bankAccountDeletedModal();
+			}
 			errorHandler(code, "", "confirmCancelPayment");
 		} 
 	}catch (err) {
